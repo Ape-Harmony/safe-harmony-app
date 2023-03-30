a456a9f8f6de854534d91c72b559cb828a7908dd
ozkrleal <ozkralm10@hotmail.com>
2023-03-30 15:07:34 -0600

    STRIPE implementation

--- packages/nextjs/components/SafeOnRamp.tsx
+++ packages/nextjs/components/SafeOnRamp.tsx
@@ -1,42 +1,48 @@
 import { SafeOnRampKit, SafeOnRampProviderType } from '@safe-global/onramp-kit'
 import { useSigner, useAccount } from "wagmi";
-import { Grid, GridItem, Button, Box } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
 import { useSafeAuth } from "../services/web3/safeAuth";
 
-export default async function SafeOnRamp() {
+export default function SafeOnRamp() {
   const { login, logout, provider, safeAuth } = useSafeAuth();
   const { data: signer }: any = useSigner();
 
-  const safeOnRamp = await SafeOnRampKit.init(SafeOnRampProviderType.Stripe, {
-    onRampProviderConfig: {
-      stripePublicKey:
-        'pk_test_51MZbmZKSn9ArdBimSyl5i8DqfcnlhyhJHD8bF2wKrGkpvNWyPvBAYtE211oHda0X3Ea1n4e9J9nh2JkpC7Sxm5a200Ug9ijfoO', // Safe public key
-      onRampBackendUrl: 'https://aa-stripe.safe.global', // Safe deployed server
-    },
-  });
+  const [data, setData] = useState(null);
+  const [safeonramp, setSafeOnRamp] = useState(null);
 
-const sessionData = await safeOnRamp.open({
-  walletAddress: useAccount().toString(),
-  networks: ['polygon'],
-  element: '#stripe-root',
-  sessionId: 'cos_1Mei3cKSn9ArdBimJhkCt1XC', // Optional, if you want to use a specific created session
-  events: {
-    onLoaded: () => console.log('Loaded'),
-    onPaymentSuccessful: () => console.log('Payment successful'),
-    onPaymentError: () => console.log('Payment failed'),
-    onPaymentProcessing: () => console.log('Payment processing')
-  }
-})
 
-  async function handleJoinSafe() {
-  //  const ethAdapter = new EthersAdapter({ ethers, signerOrProvider: signer });
-   // const safeFactory = await SafeFactory.create({ ethAdapter });
-    // TODO: join a safe
-  }
+  const fetchData = async () => {
+    // const response = await fetch("https://api.example.com/data");
+    // const json = await response.json();
+    const safeOnRamp = await SafeOnRampKit.init(SafeOnRampProviderType.Stripe, {
+      onRampProviderConfig: {
+        stripePublicKey:
+          'pk_test_51HU0zfLa65ug6JuJb5p5vR635YGUPr3msVcHKMcXOaTPBm5M1lBDFfopEVzgtresIxN2qCrrvVUizZu8EngdVYk600R6TASxVv', // Safe public key
+        onRampBackendUrl: 'https://aa-stripe.safe.global', // Safe deployed server
+      },
+    });
+    const sessionData = await safeOnRamp.open({
+      walletAddress: "0xa13fe0D873789867EE25C2471145de9c33d7F46b",
+      networks: ['ethereum'],
+      element: '#stripe-root',
+      sessionId: 'cos_1Mei3cKSn9ArdBimJhkCt1XC', // Optional, if you want to use a specific created session
+      events: {
+        onLoaded: () => console.log('Loaded'),
+        onPaymentSuccessful: () => console.log('Payment successful'),
+        onPaymentError: () => console.log('Payment failed'),
+        onPaymentProcessing: () => console.log('Payment processing')
+      }
+    })
+    // setData(sessionData);
+    // setSafeOnRamp(safeOnRamp)
+  };
+
+
+
 
   return (
-    <button>
-
+    <button onClick={fetchData}>
+        Get Stripe 
     </button>
   );
 }
