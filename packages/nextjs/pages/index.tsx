00308866590e805db37c7d61172f843ad3a4eec8
Asiya <asya.asha@gmail.com>
2023-03-31 16:56:30 -0400

    update dashboard style

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { HStack, Box, VStack, Spacer } from "@chakra-ui/react";
 
 import List from "../components/SafeList";
 
@@ -10,16 +11,27 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Safe Harmony - Home</title>
+        {/* <title>Safe Harmony - Home</title> */}
         <meta name="description" content="safe NFT lending" />
       </Head>
 
-      <div className="flex items-center flex-col flex-grow pt-8">
-        <h1 className="text-4xl font-bold">Safe Harmony</h1>
-        <p className="text-xl">safe NFT lending</p>
-        <h3>Active Safes</h3>
-        <List items={safeMock} />
-      </div>
+      <HStack spacing={8}>
+        <Box mt={4}>
+          <img src="/assets/landing.svg" alt="landing" />
+        </Box>
+        <div className="pt-8">
+          <Spacer />
+          <Box>
+            <img src="/assets/info.svg"></img>
+          </Box>
+          <Box pt={5} pb={2}>
+            <h1 style={{ fontWeight: "bold" }}>ACTIVE SAFES</h1>
+          </Box>
+          <VStack gap={2} style={{ height: "660px", overflow: "scroll" }}>
+            <List items={safeMock} />
+          </VStack>
+        </div>
+      </HStack>
     </>
   );
 };
